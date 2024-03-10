0d402e18e25106bd0515befc4ccf1e7044b14715
Zeyini Mohammad <Zeyinim@outlook.com>
2024-03-10 00:40:37 -0600

    answered quesrions 1 and 2. created variables and assigned it value.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,18 +5,19 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
 console.log('1. Create partsNeeded:');
-
+let partsNeeded = 40;
+console.log('1. Create partsNeeded:', partsNeeded);
 
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
-
-
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log('2. Create supplyChanges:',supplyChanges);
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
-
-
+ let secondItem = supplyChanges[1]
+ console.log('3. Access the second value of supplyChanges:', secondItem);
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
@@ -34,7 +35,6 @@ console.log('5. Add the value 25 into supplyChanges.');
 //      - If the value is a negative number, push it into the 'negatives' array.
 //      - If the value is a zero, push it into the 'zeroes' array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
-
 
 
 
